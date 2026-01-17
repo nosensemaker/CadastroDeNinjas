172a99a9ffdf9fbee507b6ad2b10fc213812c1d6
Yon <lethiciartt@outlook.com>
2026-01-17 17:09:34 -0300

    Create Ninja.java

---
+++ Ninja.java
@@ -0,0 +1,18 @@
+package NivelIntermediario.Desafio;
+
+public class Ninja {
+    String nome;
+    int idade;
+    String missao;
+    String nivelDificuldade;
+    String statusMissao;
+
+    public void mostrarInformacoes(){
+        System.out.println("O nome do ninja: " + nome);
+        System.out.println("A idade: " + idade);
+        System.out.println("A missão: " + missao);
+        System.out.println("A nível de dificuldade: " + nivelDificuldade);
+        System.out.println("O status da missão é:" + statusMissao);
+    }
+
+}
