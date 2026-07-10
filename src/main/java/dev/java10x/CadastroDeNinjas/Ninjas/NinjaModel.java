31d0360db1a0c9d80493ca3bd17c76642ff0ad71
nosensemake <lethiciart@outlook.com>
2026-07-09 22:30:55 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,6 +2,9 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,7 +12,10 @@ import java.util.List;
 @Entity
 // JPA = Java Persistence API
 // Para criar a tabela e dar nome a tabela
+@Data // Getters e Setters
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id // Logo o atributo que está abaixo dele vai ser o ID
@@ -25,35 +31,4 @@ public class NinjaModel {
     @JoinColumn( name = "missoes_id") // Foreing Key ou Chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel(){}
-
-    public NinjaModel(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public int getIdade(){
-        return idade;
-    }
-
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public void setIdade(int idade){
-        this.idade = idade;
-    }
 }
